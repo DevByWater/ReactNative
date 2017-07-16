326dbd048cf0d4e44b7d976d24b06ee32eb54d45
devbywater <egaraudy@gmail.com>
2017-07-15 21:31:21 -0600

    integrate react-native-elements for better ui

--- swipe/App.js
+++ swipe/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements'
 
-import Ball from './src/Ball'
+import DATA from './data/data'
+import Deck from './src/Deck'
 
 export default class App extends React.Component {
+  renderCard(item){
+    return(
+        <Card
+        key={item.id}
+        title={item.text}
+        image={{uri: item.uri}}
+        >
+          <Text style={{marginBottom: 10}}>
+            I can customize the cards
+          </Text>
+          <Button
+            icon={{name: 'code'}}
+            backgroundColor="#03A9F4"
+            title="View Now"
+          />
+        </Card>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Ball />
+        <Deck 
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
